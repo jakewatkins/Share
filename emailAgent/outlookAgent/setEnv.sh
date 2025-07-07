48719fb85032fbdb3c66793f72a0fba57d38466f
jakewatkins <jake.watkins@commonspirit.org>
2025-07-07 11:37:08 -0500

    outlookAgent works

--- emailAgent/outlookAgent/setEnv.sh
+++ emailAgent/outlookAgent/setEnv.sh
@@ -1,4 +1,4 @@
 # This script sets environment variables required for the Outlook Agent application.
 # Set environment variables for the application
-export VALLET_CLIENT_ID="c4465f79-76b9-4424-be78-fa9ec3cc3d32"
-export VALLET_SECRET="your_client_secret"
+export valetClientId="c4465f79-76b9-4424-be78-fa9ec3cc3d32"
+export valetSecret="your_client_secret"
